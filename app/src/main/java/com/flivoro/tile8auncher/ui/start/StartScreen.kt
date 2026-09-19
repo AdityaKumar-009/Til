711df63e8ed6d329afd61f72020e3ac54572da3c
Aditya Kumar <adityakumar10012@gmail.com>
2026-09-20 01:11:33 +0530

    Use AwaitPointerEventScope member without invalid import

--- app/src/main/java/com/flivoro/tile8auncher/ui/start/StartScreen.kt
+++ app/src/main/java/com/flivoro/tile8auncher/ui/start/StartScreen.kt
@@ -74,7 +74,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.PointerEventPass
-import androidx.compose.ui.input.pointer.awaitPointerEvent
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.boundsInWindow
 import androidx.compose.ui.layout.onGloballyPositioned
