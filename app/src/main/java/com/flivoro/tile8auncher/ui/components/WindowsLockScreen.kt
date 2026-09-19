5be07593e94ede6b58576f1ae40ebaa8737d4290
Aditya Kumar <adityakumar10012@gmail.com>
2026-09-19 16:46:23 +0530

    Import lock-screen parallax graphics transform

--- app/src/main/java/com/flivoro/tile8auncher/ui/components/WindowsLockScreen.kt
+++ app/src/main/java/com/flivoro/tile8auncher/ui/components/WindowsLockScreen.kt
@@ -53,6 +53,7 @@ import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onSizeChanged
